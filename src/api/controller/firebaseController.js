d66ee0254fd041c4771ba09f3684d1fac74ba7d1
Amit Kumar <sharma.amit@mounttalent.com>
2023-03-27 17:19:19 +0530

    firebase

--- src/api/controller/firebaseController.js
+++ src/api/controller/firebaseController.js
@@ -1,23 +1,30 @@
-const app = require ('../../middleware/firebase')
-const { initializeApp } =require("firebase/app");
-const { getAnalytics } =require("firebase/analytics");
-
+const { analytics, imagesRef } = require("../../middleware/firebase");
+const { getAnalytics, logEvent, isSupported } = require("firebase/analytics");
 const { user } = require("../../config/dbConnection");
+const { getStorage, ref, uploadBytes } = require("firebase/storage");
+const { getAuth } = require("firebase/auth");
+//const { getStorage } =require("firebase/storage");
 
-const analytics = getAnalytics(app);
+const createUser = async (req, res, next) => {
+  try {
+    const userData = await isSupported().then((data) => {
+      if (data) {
+        logEvent(analytics, "goal_completion", { name: req.body });
+      }
+    });
+    const data= req.body.name;
+    console.log(data);
+    const fileName =data
+    const spaceRef = ref(imagesRef, fileName);
+    const name = spaceRef.name;
+    console.log("name", name);
+    res.status(200).send({msg:'firebase send data..',data:name})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "firebase data not found", err });
+  }
+};
 
-const createUser = async(req,res,next) => {
-    const data =req.body
-    try{
-const userData = await analytics.collection('users')//.doc().set(data);
-res.status(200).send({msg:'record save successfully',data:userData})
-console.log("userData",userData);
-    }catch(err){
-        console.log(err);
-        res.status(500).send({msg:'firebase data not found',err})
-    }
-}
-
-module.exports ={
-    createUser
-}
+module.exports = {
+  createUser,
+};
--- src/middleware/firebase.js
+++ src/middleware/firebase.js
@@ -1,6 +1,31 @@
-const { initializeApp } =require("firebase/app");
-const { getAnalytics } =require("firebase/analytics");
+const { initializeApp} =require("firebase/app");
+const { getAnalytics, logEvent, isSupported } = require("firebase/analytics");
+const { Logger } =require('@firebase/logger');
 const config =require('../config/config')
+var logger = new Logger('@firebase/analytics');
 const app = initializeApp(config.firebaseConfig)
+const { getStorage, ref ,uploadBytes } =require("firebase/storage");
+const storage = getStorage(app);
 
-module.exports =app;
+if (!getStorage) {
+    console.log("data");
+    return;
+}
+//const storageRef = ref(storage);
+const storageRef = ref(storage);
+const imagesRef = ref(storageRef, 'images');
+// const fileName = "prince";
+// const spaceRef = ref(imagesRef, fileName);
+// const name = spaceRef.name;
+// console.log("spaceRef",spaceRef);
+// console.log('name',name);
+const analytics = () => {
+    if (typeof window !== "undefined") {
+      return getAnalytics(app)
+    } else {
+      return null
+    }
+  }
+module.exports = {analytics,imagesRef}
+
+  
