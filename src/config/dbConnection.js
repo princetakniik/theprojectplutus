18923da287809f253d2b2af94b8c525523245889
Amit Kumar <sharma.amit@mounttalent.com>
2023-03-29 17:10:52 +0530

    firebase

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,32 +1,49 @@
-const {Sequelize,DataTypes,QueryTypes} = require('sequelize')
-const sequelize =new Sequelize('plutos','root','root',{
-    //port:3306,
-    dialect: 'mysql',
-    host: "127.0.0.1",
-    logging: false
-})
+// const {Sequelize,DataTypes,QueryTypes} = require('sequelize')
+// const sequelize =new Sequelize('plutos','root','root',{
+//     //port:3306,
+//     dialect: 'mysql',
+//     host: "127.0.0.1",
+//     logging: false
+// })
 
-try{
-sequelize.sync().then(()=>{
-    console.log('db is connected');
-})
-}catch(err){
-    console.log('db is not connected');
-}
-//
-const db={}
-db.sequelize=sequelize;
-db.Sequelize=Sequelize;
-db.user=require('../model/userModel')(sequelize,Sequelize,DataTypes);
-db.userdetails=require('../model/userDetailsModel')(sequelize,Sequelize,DataTypes)
-db.lawOfAmendment=require('../model/lawOfAmendmentModel')(sequelize,Sequelize,DataTypes);
-db.hierarchy = require('../model/herarchyModel')(sequelize,Sequelize,DataTypes);
-db.directordetails =require('../model/DirectorModel')(sequelize,Sequelize,DataTypes);
-db.compliance = require ('../model/ComplianceModel')(sequelize,Sequelize,DataTypes);
-//Admin
-db.makeannounce = require('../model/makeAnnouncementModel')(sequelize,Sequelize,DataTypes);
-db.publishannounce = require('../model/publishAnnouncementModel')(sequelize,Sequelize,DataTypes);
-//Assine Admin User Employees
-db.assineuseremployee =require('../model/assineUserEmpModel')(sequelize,Sequelize,DataTypes);
-db.issueemp =require('../model/IssueEmpModel')(sequelize,Sequelize,DataTypes);
-module.exports = db;
+// try{
+// sequelize.sync().then(()=>{
+//     console.log('db is connected');
+// })
+// }catch(err){
+//     console.log('db is not connected');
+// }
+// //
+// const db={}
+// db.sequelize=sequelize;
+// db.Sequelize=Sequelize;
+// db.user=require('../model/userModel')(sequelize,Sequelize,DataTypes);
+// db.userdetails=require('../model/userDetailsModel')(sequelize,Sequelize,DataTypes)
+// db.lawOfAmendment=require('../model/lawOfAmendmentModel')(sequelize,Sequelize,DataTypes);
+// db.hierarchy = require('../model/herarchyModel')(sequelize,Sequelize,DataTypes);
+// db.directordetails =require('../model/DirectorModel')(sequelize,Sequelize,DataTypes);
+// db.compliance = require ('../model/ComplianceModel')(sequelize,Sequelize,DataTypes);
+// //Admin
+// db.makeannounce = require('../model/makeAnnouncementModel')(sequelize,Sequelize,DataTypes);
+// db.publishannounce = require('../model/publishAnnouncementModel')(sequelize,Sequelize,DataTypes);
+// //Assine Admin User Employees
+// db.assineuseremployee =require('../model/assineUserEmpModel')(sequelize,Sequelize,DataTypes);
+// db.issueemp =require('../model/IssueEmpModel')(sequelize,Sequelize,DataTypes);
+// module.exports = db;
+
+var mysql = require('mysql');
+var MySQLConnectionManager = require('mysql-connection-manager');
+ 
+var options = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'plutos'
+};
+ 
+var connection = mysql.createConnection(options);
+var manager = new MySQLConnectionManager(options, connection);
+//To cleanly end the current connection:
+
+manager.endConnection();
